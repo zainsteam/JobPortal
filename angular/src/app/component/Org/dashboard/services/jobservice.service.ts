e6662787fd5a84c9e3c11c03eb098715f83e8383
zainsteam <zains.team00@hotmail.com>
2019-11-14 00:01:02 +0500

    set auth org after push heroku

--- angular/src/app/component/Org/dashboard/services/jobservice.service.ts
+++ angular/src/app/component/Org/dashboard/services/jobservice.service.ts
@@ -5,28 +5,28 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 @Injectable()
 export class JobserviceService {
   private job:Job;
-  // private baseUri:string="jobs";
+  // private baseUri:string="http://localhost:3000/jobs";
   private headers = new HttpHeaders().set('Content-Type', 'application/json' );
   constructor(private http:HttpClient) { }
 
 
   createJob(job:Job){
-    return this.http.post('job/create',job,{headers:this.headers});
+    return this.http.post('http://localhost:3000/job/create',job,{headers:this.headers});
   }
 
   
   readJobs(){
-    return this.http.get('job/read',{headers:this.headers});
+    return this.http.get('http://localhost:3000/job/read',{headers:this.headers});
   }
 
   
   updateJob(job:Job){
-    return this.http.put('job/update',job,{headers:this.headers});
+    return this.http.put('http://localhost:3000/job/update',job,{headers:this.headers});
   }
 
   
   deleteJob(id:string){
-    return this.http.delete('job/delete/'+id,{headers:this.headers});
+    return this.http.delete('http://localhost:3000/job/delete/'+id,{headers:this.headers});
   }
 
   setter(job:Job){
--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://zain:Virgorian00@ds229648.mlab.com:29648/jobportal',
+    database: 'mongodb://localhost:27017/jobportal',
     secret: 'yoursecret'
 }
